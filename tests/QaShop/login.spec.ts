95f286d8e16360b025a1983c4d93b5a9cbce4d53
Angel Gutierrez <angel50710@hotmail.com>
2026-03-29 09:49:19 -0500

    add: se crea clase pom para la pagina de login de QAShop

--- tests/QaShop/login.spec.ts
+++ tests/QaShop/login.spec.ts
@@ -1,10 +1,24 @@
-import { test, expect } from '@playwright/test';
-const url ='https://www.automationtestingplatform.com/login';
+import { test, expect } from "@playwright/test";
+import { QAShopLoginPage } from "./pages/login.page";
 
-test('qaShop login', async ({ page }) => {
-    await page.goto(url);
-    await page.getByTestId('login-email-input').fill('angel50710@gmail.com');
-    await page.getByTestId('login-password-input').fill('12345678');
-    await expect(page).toHaveTitle('Login - QA Shop');
-    await expect(page.getByTestId('login-form')).toBeVisible();
+const url = "https://www.automationtestingplatform.com";
+let loginPage: QAShopLoginPage;
+test.describe("QA Shop - Login", () => {
+  test.beforeAll(async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    loginPage = new QAShopLoginPage(page);
+  });
+  test.afterAll(async () => {
+    await loginPage.page.close();
+  });
+  test.beforeEach(async () => {
+    await loginPage.goto(url);
+  });
+  test("Validate login page elements", async () => {
+    await expect(loginPage.qaShopTitle).toBeVisible();
+    await expect(loginPage.qaShopTitle).toHaveText("QA Shop");
+    await expect(loginPage.loginFormHeading).toBeVisible();
+    await expect(loginPage.loginFormHeading).toHaveText("Welcome Back");
+  });
 });
